9d63294dd999f387ee414ad0f37168681dea5fb3
Owen Leonard <owenleonard11@gmail.com>
2024-09-07 13:18:13 -0700

    Fill out contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,24 @@
+<div class="grid grid-cols-2 m-16 mt-24">
+    <div class="text-4xl font-bold">
+        general inquiry
+    </div>
+    <div class="text-xl">
+        You can reach me at my personal email 
+        <a href="mailto:owenleonard11@gmail.com" class="anchor">owenleonard11@gmail.com</a>
+        or my academic email
+        <a href="mailto:owenleonard@ucsb.edu" class="anchor">owenleonard@ucsb.edu</a>.
+        
+    </div>
+</div>
+<div class="grid grid-cols-2 m-16">
+    <div class="text-4xl font-bold">
+        code issues and contributions
+    </div>
+    <div class="text-xl">
+        If you want to report a bug, suggest a feature, or contribute code to any of my
+        projects, please look for the <code class="code text-xl">README.md</code> file in
+        the corresponding repository on my 
+        <a href="https://github.com/owenleonard11" class="anchor">Github</a>.
+        Most will have instructions under an "Issues & Contributions" header.
+    </div>
+</div>
