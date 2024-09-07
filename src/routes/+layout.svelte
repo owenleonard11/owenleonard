904ecb611b5bbc6ad1289fcd03273f9a7511ce63
Owen Leonard <owenleonard11@gmail.com>
2024-09-06 20:01:40 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import '../app.postcss';
+</script>
+
+<div class="bg-success-400 bg-[url('$lib/images/pixels.png')] p-4 text-white shadow-2xl grid grid-cols-2">
+	<div>
+		<a href="/" class="btn text-2xl">(Icon)</a>
+	</div>
+	<div class="flex flex-row justify-end">
+		<a href="/" class="btn text-2xl">home</a>
+		<a href="/projects" class="btn text-2xl">projects</a>
+		<a href="/contact" class="btn text-2xl">contact</a>
+		<a href="/about" class="btn text-2xl">about</a>
+	</div>
+</div>
+
+<slot />
+
+<div class="absolute bottom-0 grid grid-cols-1 p-4">
+	<div>
+		<span>
+			powered by <a href="https://kit.svelte.dev" class="anchor">SvelteKit</a>
+			and <a href="skeleton.dev" class="anchor">Skeleton</a>
+		</span>
+		<span>|</span>
+		<span>background textures by <a href="https://www.transparenttextures.com/" class="anchor">Transparent Textures</a></span>
+		<span>|</span>
+		<span>view <a href="/" class="anchor">page source</a> on Github</span>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+</script>
+
+<div class="space-y-5 mx-16">
+	<!-- name and bio -->
+	<div class="my-16">
+		<h1 class="text-8xl">owen leonard</h1>
+		<div class="text-4xl font-extrabold ml-16 mt-4">
+			writing, programming, and cultural criticism
+		</div>
+	</div>
+
+	<div class="grid grid-cols-3">
+		<a class="card card-hover basis-0 grow-1 shrink-1 m-4" href="/elements/cards">
+			<header>
+				IMAGE HERE
+			</header>
+			<hr class="opacity-50" />
+			<footer class="p-4">
+				<h3 class="h3 font-bold">Read my writing >>></h3>
+			</footer>
+		</a>
+		<a class="card card-hover basis-0 grow-1 shrink-1 m-4" href="/elements/cards">
+			<header>
+				IMAGE HERE
+			</header>
+			<hr class="opacity-50" />
+			<footer class="p-4">
+				<h3 class="h3 font-bold">Check out my code >>></h3>
+			</footer>
+		</a>
+		<a class="card card-hover basis-0 grow-1 shrink-1 m-4" href="/contact">
+			<header>
+				IMAGE HERE
+			</header>
+			<hr class="opacity-50" />
+			<footer class="p-4">
+				<h3 class="h3 font-bold">Get in touch >>></h3>
+			</footer>
+		</a>
+	</div>
+</div>
