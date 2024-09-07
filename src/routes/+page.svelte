dc17c4da278d06716047a5a9df78d2dab7114c54
Owen Leonard <owenleonard11@gmail.com>
2024-09-06 21:39:45 -0700

    Add other placeholders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,13 +33,50 @@
 			</div>
 		</HomeCard>
 		<HomeCard href="/projects" text="check out my code">
-			<div class="mx-8 mt-8 p-4 border-black border-t-2 border-x-2 rounded-t-md">
-				code placeholder
+			<div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-surface-500">
+				<div class="p-4 space-y-4">
+					<div class="grid grid-cols-8 gap-4">
+						<div class="placeholder bg-warning-400" />
+						<div class="placeholder bg-primary-400 col-span-2"/>
+						<div class="placeholder bg-surface-100" />
+						<div class="placeholder bg-surface-100 col-span-2" />
+					</div>
+					<div class="grid grid-cols-8 gap-4">
+						<div />
+						<div class="placeholder bg-error-400" />
+						<div class="placeholder bg-primary-400 col-span-2" />
+						<div class="placeholder bg-primary-400" />
+					</div>
+					<div class="grid grid-cols-8 gap-4">
+						<div />
+						<div class="placeholder bg-success-400 col-span-2" />
+						<div class="placeholder bg-warning-400 col-span-2" />
+						<div class="placeholder bg-success-400" />
+					</div>
+					<div class="grid grid-cols-8 gap-4">
+						<div class="placeholder bg-primary-400 col-span-2" />
+						<div class="placeholder bg-error-400 col-span-2" />
+					</div>
+				</div>
 			</div>
 		</HomeCard>
 		<HomeCard href="/projects" text="get in touch">
-			<div class="mx-8 mt-8 p-4 border-black border-t-2 border-x-2 rounded-t-md">
-				epistolary placeholder
+			<div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-warning-100">
+				<div class="p-4 space-y-4">
+					<div class="grid grid-cols-3 gap-8">
+						<div class="placeholder-circle w-14 bg-surface-500" />
+					</div>
+					<div class="grid grid-cols-4 gap-4">
+						<div />
+						<div class="placeholder bg-surface-500 col-span-2" />
+						<div />
+					</div>
+					<div class="grid grid-cols-5 gap-4">
+						<div />
+						<div class="placeholder bg-surface-500 col-span-3" />
+						<div />
+					</div>
+				</div>
 			</div>
 		</HomeCard>
 	</div>
