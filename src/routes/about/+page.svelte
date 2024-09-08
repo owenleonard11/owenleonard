0249fd60c7d3f183f68e8bc4eae6e21b8254c514
Owen Leonard <owenleonard11@gmail.com>
2024-09-08 14:55:10 -0700

    Fill out about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import OwenIcon from "$lib/components/OwenIcon.svelte";
+</script>
+
+<div class="grid gap-4 grid-cols-3 m-16 mt-24">
+    <div>
+        <OwenIcon classes="h-128 w-128 stroke-2"/>
+        <div class="mt-4 text-center w-128 text-lg px-4">
+            after Constantin Brancusi's 
+            <em>
+                <a class="anchor" href="https://www.themorgan.org/exhibitions/online/ulysses/brancusis-symbol-joyce">
+                    Symbol of James Joyce
+                </a>
+            </em>
+        </div>
+    </div>
+    <div class="text-2xl col-span-2 mr-8">
+        <p>
+            Hi! I'm Owen Leonard, a PhD student at UC Santa Barbara. 
+            My research interests include critical AI and critical machine learning, 
+            computational methods and the archive,
+            and twentieth-century modernism and its implications for the digital present.
+        </p>
+        <p class="mt-8">
+            You can find an overview of my projects on this site's
+            <a href="/projects" class="anchor">projects page</a>,
+            or go straight to
+            <a href="https://github.com/owenleonard11" class="anchor">my Github</a>
+            to find the source code and documentation.
+        </p>
+        <p class="mt-8">
+            Please <a href="/contact" class="anchor">contact me</a> for my up-to-date resume and CV or with any other questions.
+        </p>
+    </div>
+</div>
