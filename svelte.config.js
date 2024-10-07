6f7990cc8751ca7d0d6cd59adb79846a64705904
Owen Leonard <owenleonard11@gmail.com>
2024-10-06 21:31:21 -0700

    Fix base paths

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/owenleonard'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
