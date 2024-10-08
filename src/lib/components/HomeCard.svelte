38796056980c9fd7b108e7091ca08ca63e39a75d
Owen Leonard <owenleonard11@gmail.com>
2024-10-08 01:45:04 -0700

    Fix color issue

--- src/lib/components/HomeCard.svelte
+++ src/lib/components/HomeCard.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
     export let href: string
     export let text: string
-    export let bg  : string
 </script>
 
 <a class="card card-hover min-w-96 m-4 bg-primary-300 rounded-lg grow" href={href}>
     <header>
-        <div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-{bg}">
-            <slot />
-        </div>
+        <slot />
     </header>
     <hr class="opacity-50" />
     <footer class="p-4">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,70 +12,76 @@
 
 	<div class="flex flex-row flex-wrap justify-center">
 		<HomeCard href="/projects" text="read my writing" bg="warning-100">
-			<div class="p-4 space-y-4">
-				<div class="grid grid-cols-3 gap-8">
-					<div class="placeholder bg-surface-500" />
-				</div>
-				<div class="grid grid-cols-3 gap-4">
-					<div class="placeholder bg-surface-500" />
-				</div>
-				<div class="grid grid-cols-3 gap-4">
-					<div class="placeholder bg-surface-500 col-span-2" />
-					<div class="placeholder bg-surface-500" />
-				</div>
-				<div class="grid grid-cols-3 gap-4">
-					<div class="placeholder bg-surface-500" />
-					<div class="placeholder bg-surface-500 col-span-2" />
+			<div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-warning-100">
+				<div class="p-4 space-y-4">
+					<div class="grid grid-cols-3 gap-8">
+						<div class="placeholder bg-surface-500" />
+					</div>
+					<div class="grid grid-cols-3 gap-4">
+						<div class="placeholder bg-surface-500" />
+					</div>
+					<div class="grid grid-cols-3 gap-4">
+						<div class="placeholder bg-surface-500 col-span-2" />
+						<div class="placeholder bg-surface-500" />
+					</div>
+					<div class="grid grid-cols-3 gap-4">
+						<div class="placeholder bg-surface-500" />
+						<div class="placeholder bg-surface-500 col-span-2" />
+					</div>
 				</div>
 			</div>
 		</HomeCard>
 		<HomeCard href="/projects" text="check out my code" bg="surface-900">
-			<div class="p-4 space-y-4">
-				<div class="grid grid-cols-8 gap-2">
-					<div class="placeholder bg-warning-400" />
-					<div class="placeholder bg-primary-400 col-span-2"/>
-					<div class="placeholder bg-success-400" />
-					<div class="placeholder bg-success-400 col-span-2" />
+			<div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-surface-700">
+				<div class="p-4 space-y-4">
+					<div class="grid grid-cols-8 gap-2">
+						<div class="placeholder bg-warning-400" />
+						<div class="placeholder bg-primary-400 col-span-2"/>
+						<div class="placeholder bg-success-400" />
+						<div class="placeholder bg-success-400 col-span-2" />
+					</div>
+					<div class="grid grid-cols-8 gap-2">
+						<div />
+						<div class="placeholder bg-error-400" />
+						<div class="placeholder bg-primary-400 col-span-2" />
+						<div class="placeholder bg-primary-400" />
+					</div>
+					<div class="grid grid-cols-8 gap-2">
+						<div />
+						<div class="placeholder bg-success-400 col-span-2" />
+						<div class="placeholder bg-warning-400 col-span-2" />
+						<div class="placeholder bg-success-400" />
+					</div>
+					<div class="grid grid-cols-8 gap-2">
+						<div class="placeholder bg-primary-400 col-span-2" />
+						<div class="placeholder bg-error-400 col-span-2" />
+					</div>
 				</div>
-				<div class="grid grid-cols-8 gap-2">
-					<div />
-					<div class="placeholder bg-error-400" />
-					<div class="placeholder bg-primary-400 col-span-2" />
-					<div class="placeholder bg-primary-400" />
-				</div>
-				<div class="grid grid-cols-8 gap-2">
-					<div />
-					<div class="placeholder bg-success-400 col-span-2" />
-					<div class="placeholder bg-warning-400 col-span-2" />
-					<div class="placeholder bg-success-400" />
-				</div>
-				<div class="grid grid-cols-8 gap-2">
-					<div class="placeholder bg-primary-400 col-span-2" />
-					<div class="placeholder bg-error-400 col-span-2" />
-				</div>
-			</div>
+			<div />
 		</HomeCard>
 		<HomeCard href="/cv" text="explore my CV" bg="warning-100">
-			<div class="p-4 space-y-4">
-				<div class="grid grid-cols-5 gap-4">
-					<div />
-					<div class="placeholder bg-tertiary-900 col-span-3" />
-					<div />
-				</div>
-				<div class="grid grid-cols-7 gap-4">
-					<div class="col-span-2" />
-					<div class="placeholder bg-tertiary-700 col-span-3" />
-					<div class="col-span-2" />
-				</div>
-				<div class="grid grid-cols-8 gap-4">
-					<div class="placeholder bg-tertiary-700" />
-					<div class="placeholder bg-tertiary-900 col-span-2" />
-					<div class="placeholder bg-tertiary-700 col-span-4"/>
-				</div>
-				<div class="grid grid-cols-8 gap-4">
-					<div class="placeholder bg-tertiary-700" />
-					<div class="placeholder bg-tertiary-900 col-span-2" />
-					<div class="placeholder bg-tertiary-700 col-span-4"/>
+			<div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-warning-100">
+				<div class="p-4 space-y-4">
+					<div class="grid grid-cols-5 gap-4">
+						<div />
+						<div class="placeholder bg-tertiary-900 col-span-3" />
+						<div />
+					</div>
+					<div class="grid grid-cols-7 gap-4">
+						<div class="col-span-2" />
+						<div class="placeholder bg-tertiary-700 col-span-3" />
+						<div class="col-span-2" />
+					</div>
+					<div class="grid grid-cols-8 gap-4">
+						<div class="placeholder bg-tertiary-700" />
+						<div class="placeholder bg-tertiary-900 col-span-2" />
+						<div class="placeholder bg-tertiary-700 col-span-4"/>
+					</div>
+					<div class="grid grid-cols-8 gap-4">
+						<div class="placeholder bg-tertiary-700" />
+						<div class="placeholder bg-tertiary-900 col-span-2" />
+						<div class="placeholder bg-tertiary-700 col-span-4"/>
+					</div>
 				</div>
 			</div>
 		</HomeCard>
