93dd8712b9d9e9f78372160ab1511625aca177ac
Owen Leonard <owenleonard11@gmail.com>
2024-10-23 12:32:53 -0700

    Add landing responsiveness for small screens

--- src/lib/components/HomeCard.svelte
+++ src/lib/components/HomeCard.svelte
@@ -3,12 +3,12 @@
     export let text: string
 </script>
 
-<a class="card card-hover w-96 max-w-[36rem] m-4 bg-primary-300 rounded-lg grow" href={href}>
+<a class="card card-hover w-96 max-w-[32rem] m-4 bg-primary-300 rounded-lg grow" href={href}>
     <header>
         <slot />
     </header>
     <hr class="opacity-50" />
-    <footer class="p-4">
-        <h3 class="h3 font-bold mx-4">{text} →</h3>
+    <footer class="p-2 md:p-4">
+        <p class="text-xl md:text-2xl lg:text-3xl font-bold mx-4">{text} →</p>
     </footer>
 </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,28 +3,28 @@
 </script>
 
 <div class="mx-8 lg:mx-16">
-	<div class="text-center mt-16 mx-16">
-		<h1 class="text-6xl lg:text-8xl font-bold">owen leonard</h1>
-		<div class="text-3xl lg:text-4xl font-extrabold mt-4">
+	<div class="mt-4 text-center md:mt-16 font-serif">
+		<h1 class="text-4xl font-bold md:font-normal md:text-7xl lg:text-8xl">owen leonard</h1>
+		<div class="text-xl md:text-3xl lg:text-4xl font-extrabold mt-4">
 			writing, programming, and cultural criticism
 		</div>
 	</div>
 
-	<div class="flex flex-row flex-wrap justify-center mt-8 lg:mt-16">
+	<div class="flex flex-row flex-wrap justify-center mt-4 md:mt-8">
 		<HomeCard href="/projects" text="read my writing">
-			<div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-warning-100">
-				<div class="p-4 space-y-4">
-					<div class="grid grid-cols-3 gap-8">
+			<div class="mx-4 md:mx-8 mt-4 md:mt-8 md:p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-warning-100">
+				<div class="p-4 pb-1 md:pb-0 space-y-2 md:space-y-4">
+					<div class="grid grid-cols-3">
 						<div class="placeholder bg-surface-500" />
 					</div>
-					<div class="grid grid-cols-3 gap-4">
+					<div class="grid grid-cols-3">
 						<div class="placeholder bg-surface-500" />
 					</div>
-					<div class="grid grid-cols-3 gap-4">
+					<div class="grid grid-cols-3 gap-2 md:gap-4">
 						<div class="placeholder bg-surface-500 col-span-2" />
 						<div class="placeholder bg-surface-500" />
 					</div>
-					<div class="grid grid-cols-3 gap-4">
+					<div class="grid grid-cols-3 gap-2 md:gap-4">
 						<div class="placeholder bg-surface-500" />
 						<div class="placeholder bg-surface-500 col-span-2" />
 					</div>
@@ -32,27 +32,27 @@
 			</div>
 		</HomeCard>
 		<HomeCard href="/projects" text="check out my code">
-			<div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-surface-700">
-				<div class="p-4 space-y-4">
-					<div class="grid grid-cols-8 gap-2">
+			<div class="mx-4 md:mx-8 mt-4 md:mt-8 md:p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-surface-700">
+				<div class="p-4 pb-1 md:pb-0 space-y-2 md:space-y-4">
+					<div class="grid grid-cols-8 gap-1 md:gap-2">
 						<div class="placeholder bg-warning-400" />
 						<div class="placeholder bg-primary-400 col-span-2"/>
 						<div class="placeholder bg-success-400" />
 						<div class="placeholder bg-success-400 col-span-2" />
 					</div>
-					<div class="grid grid-cols-8 gap-2">
+					<div class="grid grid-cols-8 gap-1 md:gap-2">
 						<div />
 						<div class="placeholder bg-error-400" />
 						<div class="placeholder bg-primary-400 col-span-2" />
 						<div class="placeholder bg-primary-400" />
 					</div>
-					<div class="grid grid-cols-8 gap-2">
+					<div class="grid grid-cols-8 gap-1 md:gap-2">
 						<div />
 						<div class="placeholder bg-success-400 col-span-2" />
 						<div class="placeholder bg-warning-400 col-span-2" />
 						<div class="placeholder bg-success-400" />
 					</div>
-					<div class="grid grid-cols-8 gap-2">
+					<div class="grid grid-cols-8 gap-1 md:gap-2">
 						<div class="placeholder bg-primary-400 col-span-2" />
 						<div class="placeholder bg-error-400 col-span-2" />
 					</div>
@@ -60,27 +60,27 @@
 			<div />
 		</HomeCard>
 		<HomeCard href="/cv" text="view my CV">
-			<div class="mx-8 mt-8 p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-warning-100">
-				<div class="p-4 space-y-4">
-					<div class="grid grid-cols-5 gap-4">
+			<div class="mx-4 md:mx-8 mt-4 md:mt-8 md:p-4 pb-0 border-black border-t-2 border-x-2 rounded-t-md bg-warning-100">
+				<div class="p-4 pb-1 md:pb-0 space-y-2 md:space-y-4">
+					<div class="grid grid-cols-5 gap-2 md:gap-4">
 						<div />
 						<div class="placeholder bg-tertiary-900 col-span-3" />
 						<div />
 					</div>
-					<div class="grid grid-cols-7 gap-4">
+					<div class="grid grid-cols-7 gap-2 md:gap-4">
 						<div class="col-span-2" />
 						<div class="placeholder bg-tertiary-700 col-span-3" />
 						<div class="col-span-2" />
 					</div>
-					<div class="grid grid-cols-8 gap-4">
+					<div class="grid grid-cols-8 gap-2">
 						<div class="placeholder bg-tertiary-700" />
-						<div class="placeholder bg-tertiary-900 col-span-2" />
+						<div class="placeholder bg-tertiary-900 col-span-2 md:mr-1" />
 						<div class="placeholder bg-tertiary-700 col-span-4"/>
 					</div>
-					<div class="grid grid-cols-8 gap-4">
+					<div class="grid grid-cols-8 gap-2">
 						<div class="placeholder bg-tertiary-700" />
-						<div class="placeholder bg-tertiary-900 col-span-2" />
-						<div class="placeholder bg-tertiary-700 col-span-4"/>
+						<div class="placeholder bg-tertiary-900 col-span-2 md:mr-1" />
+						<div class="placeholder bg-tertiary-700 col-span-5"/>
 					</div>
 				</div>
 			</div>
