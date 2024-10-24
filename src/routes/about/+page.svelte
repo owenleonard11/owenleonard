32bcfca7744cb96d15159110e3ec17bc3be239ef
Owen Leonard <owenleonard11@gmail.com>
2024-10-23 18:00:05 -0700

    Make about page responsive for sm and md

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
+    import { base } from "$app/paths";
     import OwenIcon from "$lib/components/OwenIcon.svelte";
 </script>
 
-<div class="flex flex-row m-16 mt-24">
-    <div class="mr-8">
-        <OwenIcon classes="h-[32rem] w-[32rem] stroke-[4px]"/>
-        <div class="mt-4 text-center w-128 text-lg px-4">
+<div class="flex flex-row m-8 md:m-12 md:mt-16 lg:m-16 lg:mt-24">
+    <div class="hidden md:block mr-8">
+        <OwenIcon classes="h-[30dvw] w-[30dvw] stroke-[4px]"/>
+        <div class="mt-4 text-center w-128 text-lg xl:text-xl px-4">
             after Constantin Brancusi's 
-            <em>
-                <a class="anchor" href="https://www.themorgan.org/exhibitions/online/ulysses/brancusis-symbol-joyce">
-                    Symbol of James Joyce
-                </a>
-            </em>
+            <em><a class="anchor" href="https://www.themorgan.org/exhibitions/online/ulysses/brancusis-symbol-joyce">
+                Symbol of James Joyce
+            </a></em>
         </div>
     </div>
-    <div class="text-2xl col-span-2 mr-8">
+    <div class="text-l md:text-xl xl:text-2xl col-span-2 mr-8">
         <p>
             Hi! I'm Owen Leonard, a PhD student at UC Santa Barbara. 
             My research interests include critical AI and critical machine learning, 
             computational methods and the archive,
             and twentieth-century modernism and its implications for the digital present.
+            You can find my <a class="anchor" href="{base}/cv">interactive CV here</a>.
         </p>
         <p class="mt-8">
             You can find an overview of my projects on this site's
@@ -27,12 +27,9 @@
             or go straight to
             <a target="_blank" href="https://github.com/owenleonard11" class="anchor">my Github</a>
             to find the source code and documentation.
-            <a href="/disjecta" class="anchor">Disjecta</a>
+            <em><a href="/disjecta" class="anchor">Disjecta</a></em>
             is my intermittently updated blog, where I post shorter casual writings
             that don't fit into other projects.
-        </p>
-        <p class="mt-8">
-            Please <a href="/contact" class="anchor">contact me</a> for my up-to-date resume and CV or with any other questions.
         </p>
         <p class="mt-8">
             This site is powered by <a target="_blank" href="https://kit.svelte.dev" class="anchor">SvelteKit</a>
