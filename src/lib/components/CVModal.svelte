2dea4f0c82187c531681ff9a611ada7665877d18
Owen Leonard <owenleonard11@gmail.com>
2024-10-23 20:29:34 -0700

    Fix button selection box in modal component

--- src/lib/components/CVModal.svelte
+++ src/lib/components/CVModal.svelte
@@ -10,8 +10,8 @@
 {#if $modalStore[0]}
     <div class="card p-4 text-lg bg-secondary-100 w-modal flex flex-row justify-between align-center border-tertiary-600 border-2 rounded-md">
         <div>{side}</div>
-        <button on:click={() => modalStore.close()}>
-			<Icon src={ChevronDoubleDown} class='w-8 ml-8'/>
+        <button on:click={() => modalStore.close()} class="ml-4">
+			<Icon src={ChevronDoubleDown} class='w-8'/>
 		</button>
     </div>
 {/if}
