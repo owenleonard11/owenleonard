a014fd50fd57301592423250d765fd1e9499f5f7
Owen Leonard <owenleonard11@gmail.com>
2024-10-23 17:41:22 -0700

    Make contact page responsive for sm and md

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,22 +1,22 @@
-<div class="grid grid-cols-2 m-16 mt-24">
-    <div class="text-4xl font-bold">
+<div class="flex flex-col md:flex-row justify-between m-8 md:m-12 md:mt-16 lg:m-16 lg:mt-24">
+    <div class="text-2xl md:text-3xl lg:text-4xl md:w-[26rem] lg:w-[48rem] font-bold text-nowrap mr-8">
         general inquiry
     </div>
-    <div class="text-xl">
+    <div class="text-l md:text-xl w-full">
         You can reach me at my personal email 
         <a href="mailto:owenleonard11@gmail.com" class="anchor">owenleonard11@gmail.com</a>
         or my academic email
         <a href="mailto:owenleonard@ucsb.edu" class="anchor">owenleonard@ucsb.edu</a>.
-        
     </div>
 </div>
-<div class="grid grid-cols-2 m-16">
-    <div class="text-4xl font-bold">
-        code issues and contributions
+
+<div class="flex flex-col md:flex-row justify-between m-8 md:m-12 md:mt-16 lg:m-16 lg:mt-24">
+    <div class="text-2xl md:text-3xl lg:text-4xl md:w-[26rem] lg:w-[48rem] font-bold mr-8">
+        <span class="whitespace-nowrap">code issues and</span> contributions
     </div>
-    <div class="text-xl">
+    <div class="text-l md:text-xl w-full">
         If you want to report a bug, suggest a feature, or contribute code to any of my
-        projects, please look for the <code class="code text-xl">README.md</code> file in
+        projects, please look for the <code class="code text-l md:text-xl">README.md</code> file in
         the corresponding repository on my 
         <a target="_blank" href="https://github.com/owenleonard11" class="anchor">Github</a>.
         Most will have instructions under an "Issues & Contributions" header.
