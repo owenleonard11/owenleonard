2582c4442550487b31db3379488b3dceb72ba7cb
Owen Leonard <owenleonard11@gmail.com>
2024-11-08 21:03:58 -0800

    Minor about page formatting fixes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,7 +15,7 @@
         </div>
     </div>
     <div class="md:text-xl lg:text-2xl md:mr-8">
-        <div class="rounded-lg p-2 lg:p-4 bg-primary-400 border-primary-700 border-2 font-medium">
+        <div class="rounded-lg p-2 lg:p-4 bg-primary-400 border-primary-700 border-2">
             <p>
                 I'm Owen Leonard, a PhD student in English at UC Santa Barbara. 
                 My research interests include critical AI and critical machine learning, 
@@ -26,40 +26,40 @@
             <dl class="
             list-dl mt-4 inline-block 
             [&_a]:card-hover [&_a]:flex [&_a]:p-2 [&_a]:pr-4 [&_a]:border-t-2 [&_a]:border-primary-700 last:[&_a]:border-b-2
-            [&_i]:text-2xl [&_i]:w-8 [&_i]:md:text-4xl [&_i]:md:w-10
+            [&_i]:text-2xl [&_i]:w-8 [&_i]:md:text-3xl [&_i]:lg:text-4xl [&_i]:md:w-10
             [&_dt]:text-left [&_dt]:font-bold [&_dt]:text-base [&_dt]:lg:text-xl
             [&_dd]:text-xs [&_dd]:md:text-sm [&_dd]:lg:text-lg
         ">
             <a href="{base}/cv" class="border-t-2 border-primary-700">
-                <span class="badge mr-1 md:mr-2 text-center"><i class="fa-regular fa-file"/></span>
+                <span class="badge p-0 mr-2 lg:mr-4 text-center"><i class="fa-regular fa-file"/></span>
                 <span>
                     <dt>View my CV →</dt>
                     <dd>See my interactive online CV, or download a PDF</dd>
                 </span>
             </a>
             <a href="{base}/writing">
-                <span class="badge mr-1 md:mr-2 text-center"><i class="fa-solid fa-book-open"/></span>
+                <span class="badge p-0 mr-2 lg:mr-4 text-center"><i class="fa-solid fa-book-open"/></span>
                 <span>
                     <dt>Read my writing →</dt>
                     <dd>Find my latest academic writing</dd>
                 </span>
             </a>
             <a href="{base}/projects">
-                <span class="badge mr-1 md:mr-2 text-center"><i class="fa-regular fa-folder"/></span>
+                <span class="badge p-0 mr-2 lg:mr-4 text-center"><i class="fa-regular fa-folder"/></span>
                 <span>
                     <dt>Check out my projects →</dt>
                     <dd>Browse my writing and programming projects</dd>
                 </span>
             </a>
             <a href="https://github.com/owenleonard11" target="_blank">
-                <span class="badge mr-1 md:mr-2 text-center"><i class="fa-brands fa-github"/></span>
+                <span class="badge p-0 mr-2 lg:mr-4 text-center"><i class="fa-brands fa-github"/></span>
                 <span>
                     <dt>Visit my Github page →</dt>
                     <dd>Explore and download my open-source code</dd>
                 </span>
             </a>
             <a href="https://orcid.org/0009-0007-4386-5628" target="_blank">
-                <span class="badge mr-1 md:mr-2 text-center"><i class="fa-brands fa-orcid"/></span>
+                <span class="badge p-0 mr-2 lg:mr-4 text-center"><i class="fa-brands fa-orcid"/></span>
                 <span>
                     <dt>Visit my ORCID profile →</dt>
                     <dd>Find my scholarly publications</dd>
