26658ffadbd157d8fb851a3b84e9291cd30df625
Owen Leonard <owenleonard11@gmail.com>
2024-11-08 21:52:43 -0800

    Fix projects column width

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,7 +7,7 @@
     <div class="flex flex-col gap-4 lg:gap-8 w-full lg:w-1/2">
         <ProjectCard project={projects.huml_site}/>
     </div>
-    <div class="flex flex-col gap-4 lg:gap-8 w-full w-1/2">
+    <div class="flex flex-col gap-4 lg:gap-8 w-full lg:w-1/2">
 
     </div>
 </div>
