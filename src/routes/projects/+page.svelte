9fd58644cae0da7769c60d375f767290bb0221f5
Owen Leonard <owenleonard11@gmail.com>
2024-11-08 21:06:05 -0800

    Remove cards for unfinished projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,11 +4,10 @@
 </script>
 
 <div class="flex flex-row md:mx-16 lg:mx-32 xl:mx-64 mt-4 md:mt-8 lg:mt-16 gap-4 lg:gap-8 mb-4">
-    <div class="flex flex-col gap-4 lg:gap-8 w-full w-1/2">
-        <ProjectCard project={projects.glowing_brains} />
-        <ProjectCard project={projects.ai_infrastructure} />
-    </div>
     <div class="flex flex-col gap-4 lg:gap-8 w-full lg:w-1/2">
         <ProjectCard project={projects.huml_site}/>
+    </div>
+    <div class="flex flex-col gap-4 lg:gap-8 w-full w-1/2">
+
     </div>
 </div>
