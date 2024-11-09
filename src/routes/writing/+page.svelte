52e201fba4ccc8f46ea686615c67746b6db9bf2f
Owen Leonard <owenleonard11@gmail.com>
2024-11-08 20:43:40 -0800

    Add message to writing page

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -5,6 +5,13 @@
 </script>
 
 <div class="bg-primary-400 m-4 md:m-8 md:mt-12 lg:m-12 lg:mt-16 lg:p-2 rounded-lg border-2 border-primary-600">
+    <div class="m-4 text-xl">
+        <p>
+            Below you will find a selection of my academic writing, sorted from newest to oldest. Click the titles to see a brief summary
+            and download a PDF where possible.
+        </p>
+    </div>
+    <div class="border-t-2 border-primary-600 mx-4"></div>
     <Accordion>
         <WritingCard writing={writings.ulysses_supercomputer} />
         <WritingCard writing={writings.drama_life_dedalus} />
