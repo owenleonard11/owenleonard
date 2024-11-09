c9985404dd8d0a22feeeebea68ae1d3d3eb381b1
Owen Leonard <owenleonard11@gmail.com>
2024-11-08 20:38:35 -0800

    Add Joyce thesis to writing page

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -6,6 +6,7 @@
 
 <div class="bg-primary-400 m-4 md:m-8 md:mt-12 lg:m-12 lg:mt-16 lg:p-2 rounded-lg border-2 border-primary-600">
     <Accordion>
-        <WritingCard writing={writings.ulysses_supercomputer}/>
+        <WritingCard writing={writings.ulysses_supercomputer} />
+        <WritingCard writing={writings.drama_life_dedalus} />
     </Accordion>
 </div>
