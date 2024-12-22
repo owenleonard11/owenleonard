0b9f7000fc9b53fbe9f03295c6a56ee3e326fa76
Owen Leonard <owenleonard11@gmail.com>
2024-12-21 22:00:53 -0500

    Redo CV PDF design

--- scripts/update_cv_tex.py
+++ scripts/update_cv_tex.py
@@ -1,50 +1,30 @@
-# tested on python 3.9, 3.10
-
 from json import load
 
-TEX_HEAD = '\\documentclass[a4paper,10pt]{article}\n'
-TEX_PACKAGES = [
-    ('fontawesome', ''),
-    ('inputenc', 'utf8'),
-    ('fontenc', 'T1'),
-    ('lmodern', ''),
-    ('geometry', 'left=1cm, right=1cm, top=1.5cm, bottom=1cm'),
-    ('hyperref', 'colorlinks=true, urlcolor=blue'),
-    ('parskip', ''),
-    ('titlesec', ''),
-    ('xcolor', 'dvipsnames')
-]  
-TEX_SPACING = '\\titlespacing{\\section}{0pt}{\\parskip}{1\\parskip}\n\n'
+def brk(text: str):
+    return f'{{{text}}}'
 
-def stz(text: str):
+def esc(text: str):
     return text.replace('&', '\\&')
 
-with open('src/lib/text/cv.json') as cv_json:
+with open('src/lib/text/cv_text.json') as cv_json:
     cv = load(cv_json)
 
-with open('src/lib/text/cv.tex', 'w') as tex:
-    tex.write(TEX_HEAD)
-    for package, options in TEX_PACKAGES:
-        tex.write(f'\\usepackage[{options}]{{' + package + '}\n')
-    tex.write(TEX_SPACING)
-    tex.write('\\begin{document}\n')
-
-    # Name & Contact Info
-    tex.write('{\\LARGE \\textbf{Owen Leonard}} \\\\[0.2cm]\n')
-    tex.write('\\noindent\n')
-    tex.write('\\faEnvelope \\hspace{0.2cm} \\href{mailto:owenleonard@ucsb.edu}{owenleonard@ucsb.edu} \\\\\n')
-    tex.write('\\faLink \\hspace{0.2cm} \\href{https://owenleonard.dev/}{owenleonard.dev} \\\\\n')
-    tex.write('\\faGithub \\hspace{0.2cm} \\href{https://github.com/owenleonard11}{github.com/owenleonard11}\n\n')
-
+with open('src/lib/text/cv_base.tex', 'a') as tex:
     for section in cv['sections']:
-        tex.write('\\section*{' + stz(section['title']) + '}\n\n')
+        tex.write(f'\t\\cvsection{brk(esc(section["title"]))}\n')
         for entry in section['entries']:
-            tex.write('\\textbf{' + stz(entry['left']) + '} ' if entry['bold'] == 'left' else stz(entry['left']) + ' ')
-            if entry['alt']:
-                tex.write('\\hspace{0.2cm} \\textcolor{darkgray}{\\textit{' + stz(entry["alt"]) + '}} ')
-            tex.write('\\hfill ')
-            tex.write('textbf{' + stz(entry['right']) + ' }' if entry['bold'] == 'right' else stz(entry['right']) + ' ')
-            tex.write('\n\n')
-        tex.write('\n')
-            
+            if 'date' not in entry:
+                print(entry)
+            date = brk(esc(entry['date']))
+            bold = brk(esc(entry['bold']))
+            norm = esc(entry['norm']) if 'norm' in entry else ''
+            undr = brk(esc(entry['undr'])) if 'undr' in entry else ''
+            href = brk(entry["href"]) if 'href' in entry else ''
+            if 'href' in entry:
+                text = brk(f'\\textbf{bold}{norm} \\faExternalLink')
+                top_line = brk(f'\\href{href}{text}')
+            else:
+                top_line = brk(f'\\textbf{bold}{norm}')
+            tex.write(f'\t\t\\{"double" if undr else "single"}entry{date}{top_line}{undr}\n')
+        tex.write('\n\t\\vspace{0.5cm}\n')
     tex.write('\\end{document}')
