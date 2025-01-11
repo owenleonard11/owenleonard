c1b3c781f7f93be48ee8e15e901895af8125d047
Owen Leonard <owenleonard11@gmail.com>
2025-01-10 16:23:22 -0800

    Add g2c and report to projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,9 +5,10 @@
 
 <div class="flex flex-col lg:flex-row m-4 md:m-8 md:mt-12 lg:mx-16 lg:mt-16 xl:mx-32 xl:mt-24 gap-4 lg:gap-8 mb-4">
     <div class="flex flex-col gap-4 lg:gap-8 w-full lg:w-1/2">
-        <ProjectCard project={projects.huml_site}/>
+        <ProjectCard project={projects.g2c} />
+        <ProjectCard project={projects.huml_site} />
     </div>
     <div class="flex flex-col gap-4 lg:gap-8 w-full lg:w-1/2">
-        
+        <ProjectCard project={projects.huml_report} />
     </div>
 </div>
